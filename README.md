341b6995e0a262885b0c1697c620c47f5f73312d
Mau <mau@qmau.me>
2022-11-11 14:28:46 +0900

    added scheduled fargate task

--- README.md
+++ README.md
@@ -1,58 +1,31 @@
 
-# Welcome to your CDK Python project!
+# Tác vụ Fargate được lập lịch (Scheduled Fargate Tasks) với AWS CDK
 
-This is a blank project for CDK development with Python.
+## Chạy project
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-This project is set up like a standard Python project.  The initialization
-process also creates a virtualenv within this project, stored under the `.venv`
-directory.  To create the virtualenv it assumes that there is a `python3`
-(or `python` for Windows) executable in your path with access to the `venv`
-package. If for any reason the automatic creation of the virtualenv fails,
-you can create the virtualenv manually.
-
-To manually create a virtualenv on MacOS and Linux:
+- Clone project
 
 ```
-$ python3 -m venv .venv
+git clone https://github.com/vntechies/011-aws-cdk-scheduled-fargate-task.git aws-cdk-scheduled-fargate-task
 ```
 
-After the init process completes and the virtualenv is created, you can use the following
-step to activate your virtualenv.
+- Tạo virtualenv
 
-```
-$ source .venv/bin/activate
-```
-
-If you are a Windows platform, you would activate the virtualenv like this:
-
-```
-% .venv\Scripts\activate.bat
-```
-
-Once the virtualenv is activated, you can install the required dependencies.
-
-```
-$ pip install -r requirements.txt
+```shell
+cd aws-cdk-scheduled-fargate-task
+python3 -m venv .venv
+source .venv/bin/activate
+# Trên windows
+.venv\Scripts\activate.bat
+pip install -r requirements.txt
+cdk synth
 ```
 
-At this point you can now synthesize the CloudFormation template for this code.
+## Một số câu lệnh hữu ích 
 
-```
-$ cdk synth
-```
+ * `cdk ls`          list tất cả các stacks trong ứng dụng
+ * `cdk synth`       tạo ra các CloudFormation template tổng hợp
+ * `cdk deploy`      triển khai stack này lên tài khoản và region AWS mặc định
+ * `cdk diff`        so sánh stack đã triển khai với hiện tại
+ * `cdk docs`        mở tài liệu CDK
 
-To add additional dependencies, for example other CDK libraries, just add
-them to your `setup.py` file and rerun the `pip install -r requirements.txt`
-command.
-
-## Useful commands
-
- * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk docs`        open CDK documentation
-
-Enjoy!
--- aws_cdk_scheduled_fargate_task/aws_cdk_scheduled_fargate_task_stack.py
+++ aws_cdk_scheduled_fargate_task/aws_cdk_scheduled_fargate_task_stack.py
@@ -1,19 +1,69 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns,
+    aws_applicationautoscaling as appscaling,
+    aws_logs,
 )
+
 from constructs import Construct
 
-class AwsCdkScheduledFargateTaskStack(Stack):
 
+class AwsCdkScheduledFargateTaskStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Tạo VPC với 9 subnets trên 3 AZs
+        my_vpc = ec2.Vpc(
+            self,
+            "myvpc",
+            cidr="172.31.0.0/16",
+            max_azs=3,
+            nat_gateways=1,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    cidr_mask=20, name="public", subnet_type=ec2.SubnetType.PUBLIC
+                ),
+                ec2.SubnetConfiguration(
+                    cidr_mask=20,
+                    name="application",
+                    subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT,
+                ),
+                ec2.SubnetConfiguration(
+                    cidr_mask=20,
+                    name="data",
+                    subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+                    reserved=True,
+                ),
+            ],
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkScheduledFargateTaskQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Tạo ECS Cluster
+        my_cluster = ecs.Cluster(
+            self,
+            "service-cluster",
+            cluster_name="service-cluster",
+            container_insights=True,
+            vpc=my_vpc,
+        )
+
+        image = ecs.ContainerImage.from_registry("amazonlinux:2")
+
+        ecs_patterns.ScheduledFargateTask(
+            self,
+            "amazon-linux-echo-task",
+            cluster=my_cluster,
+            platform_version=ecs.FargatePlatformVersion.LATEST,
+            schedule=appscaling.Schedule.expression("rate(1 minute)"),
+            scheduled_fargate_task_image_options=ecs_patterns.ScheduledFargateTaskImageOptions(
+                image=image,
+                memory_limit_mib=1024,
+                log_driver=ecs.LogDriver.aws_logs(
+                    stream_prefix="scheduled-fargate-task",
+                    log_retention=aws_logs.RetentionDays.ONE_DAY,
+                ),
+                environment={"APP_NAME": "scheduled-fargate-task"},
+                command=["echo", "Xin chào VNTechies!"],
+            ),
+        )
